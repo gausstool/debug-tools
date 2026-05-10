71382c00bdb441865291f07eb8ce2e96171c2dab
gausszhou <gausszhou@qq.com>
2026-05-11 00:18:49 +0800

    feat: Lorem实现符合标准(扩展词汇+标准段落)

--- src/domain/transform/modules/random/random-lorem.ts
+++ src/domain/transform/modules/random/random-lorem.ts
@@ -1,26 +1,107 @@
 const LOREM_WORDS = [
+  'lorem', 'ipsum', 'dolor', 'sit', 'amet', 'consectetur', 'adipiscing', 'elit', 'sed',
+  'do', 'eiusmod', 'tempor', 'incididunt', 'ut', 'labore', 'et', 'dolore', 'magna',
+  'aliqua', 'enim', 'ad', 'minim', 'veniam', 'quis', 'nostrud', 'exercitation',
+  'ullamco', 'laboris', 'nisi', 'aliquip', 'ex', 'ea', 'commodo', 'consequat', 'duis',
+  'aute', 'irure', 'in', 'reprehenderit', 'voluptate', 'velit', 'esse', 'cillum',
+  'fugiat', 'nulla', 'pariatur', 'excepteur', 'sint', 'occaecat', 'cupidatat', 'non',
+  'proident', 'sunt', 'culpa', 'qui', 'officia', 'deserunt', 'mollit', 'anim', 'id',
+  'est', 'laborum', 'perspiciatis', 'unde', 'omnis', 'iste', 'natus', 'error',
+  'voluptatem', 'accusantium', 'doloremque', 'laudantium', 'totam', 'rem', 'aperiam',
+  'eaque', 'ipsa', 'quae', 'ab', 'illo', 'inventore', 'veritatis', 'quasi',
+  'architecto', 'beatae', 'vitae', 'dicta', 'sunt', 'explicabo', 'nemo', 'ipsam',
+  'quia', 'voluptas', 'aspernatur', 'aut', 'odit', 'fugit', 'consequuntur', 'magni',
+  'dolores', 'eos', 'qui', 'ratione', 'sequi', 'nesciunt', 'neque', 'porro',
+  'quisquam', 'numquam', 'eius', 'modi', 'tempora', 'incidunt', 'magnam', 'quaerat',
+  'minima', 'nostrum', 'exercitationem', 'corporis', 'suscipit', 'laboriosam',
+  'aliquid', 'commodi', 'consequatur', 'autem', 'vel', 'iure', 'reprehenderit',
+  'quam', 'nihil', 'molestiae', 'illum', 'quo', 'voluptas', 'atvero', 'eos',
+  'accusamus', 'iusto', 'odio', 'dignissimos', 'ducimus', 'blanditiis', 'praesentium',
+  'voluptatum', 'deleniti', 'corrupti', 'quos', 'dolores', 'quas', 'molestias',
+  'excepturi', 'sint', 'cupiditate', 'provident', 'similique', 'culpa', 'officia',
+  'mollitia', 'animi', 'laborum', 'dolor', 'fuga', 'harum', 'quidem', 'rerum',
+  'facilis', 'expedita', 'distinctio', 'nam', 'libero', 'tempore', 'cum', 'soluta',
+  'nobis', 'eligendi', 'optio', 'cumque', 'nihil', 'impedit', 'quo', 'minus', 'id',
+  'maxime', 'placeat', 'facere', 'possimus', 'omnis', 'voluptas', 'assumenda',
+  'omnis', 'dolor', 'repellendus', 'temporibus', 'autem', 'officiis', 'debitis',
+  'rerum', 'necessitatibus', 'saepe', 'eveniet', 'voluptates', 'repudiandae',
+  'recusandae', 'itaque', 'earum', 'hic', 'tenetur', 'sapiente', 'delectus',
+  'reiciendis', 'voluptatibus', 'maiores', 'alias', 'perferendis', 'doloribus',
+  'asperiores', 'repellat', 'integer', 'libero', 'felis', 'massa', 'justo', 'odio',
+  'cursus', 'metus', 'vitae', 'ultricies', 'praesent', 'lectus', 'tortor',
+  'condimentum', 'lacinia', 'quis', 'vel', 'eros', 'donec', 'sollicitudin', 'tellus',
+  'cras', 'blandit', 'mi', 'porta', 'lacinia', 'sapien', 'quisque', 'porta', 'nisl',
+  'eget', 'eleifend', 'quam', 'pellentesque', 'diam', 'arcu', 'curabitur',
+  'aliquam', 'nibh', 'suscipit', 'at', 'semper', 'interdum', 'malesuada', 'fames',
+  'ante', 'primis', 'faucibus', 'orci', 'luctus', 'ultrices', 'posuere', 'cubilia',
+  'curae', 'mauris', 'viverra', 'morbi', 'nam', 'tincidunt', 'lacus', 'nunc',
+  'molestie', 'elementum', 'tempus', 'risus', 'facilisi', 'fermentum', 'pretium',
+  'hendrerit', 'ante', 'suspendisse', 'potenti', 'leo', 'eget', 'vitae', 'pretium',
+  'tortor', 'condimentum', 'risus', 'nulla', 'facilisi', 'cras', 'fermentum',
+  'odio', 'feugiat', 'pretium', 'libero', 'nunc', 'hendrerit', 'suspendisse',
+  'potenti', 'sed', 'leo', 'eget', 'magna', 'semper', 'vitae', 'porta', 'lorem',
+  'ipsum', 'neque', 'porro', 'quisquam', 'est', 'qui', 'dolorem', 'ipsum', 'quia',
+  'dolor', 'amet', 'consectetur', 'adipisci', 'velit', 'sed', 'quia', 'non',
+  'numquam', 'eius', 'modi', 'tempora', 'incidunt', 'labore', 'dolore', 'magnam',
+  'aliquam', 'quaerat', 'voluptatem', 'enim', 'ad', 'minima', 'veniam', 'quis',
+  'nostrum', 'exercitationem', 'ullam', 'corporis', 'suscipit', 'laboriosam', 'nisi',
+  'aliquid', 'ex', 'ea', 'commodi', 'consequatur', 'quis', 'autem', 'vel', 'eum',
+  'iure', 'reprehenderit', 'qui', 'ea', 'voluptate', 'velit', 'esse', 'quam',
+  'nihil', 'molestiae', 'consequatur', 'vel', 'illum', 'qui', 'dolorem', 'eum',
+  'fugiat', 'quo', 'voluptas', 'nulla', 'pariatur', 'at', 'vero', 'eos', 'accusamus',
+  'iusto', 'odio', 'ducimus', 'blanditiis', 'praesentium', 'voluptatum', 'deleniti',
+  'atque', 'corrupti', 'quos', 'dolores', 'quas', 'molestias', 'excepturi',
+  'sint', 'occaecati', 'cupiditate', 'non', 'provident', 'similique', 'sunt', 'in',
+  'culpa', 'qui', 'officia', 'deserunt', 'mollitia', 'animi', 'id', 'est',
+  'laborum', 'et', 'dolor', 'fuga', 'et', 'harum', 'quidem', 'rerum', 'facilis',
+  'est', 'expedita', 'distinctio', 'nam', 'libero', 'tempore', 'cum', 'soluta',
+  'nobis', 'est', 'eligendi', 'optio', 'cumque', 'nihil', 'impedit', 'quo', 'minus',
+  'id', 'quod', 'maxime', 'placeat', 'facere', 'possimus', 'omnis', 'voluptas',
+  'assumenda', 'omnis', 'dolor', 'repellendus', 'temporibus', 'autem', 'quibusdam',
+  'aut', 'officiis', 'debitis', 'aut', 'rerum', 'necessitatibus', 'saepe',
+  'eveniet', 'ut', 'et', 'voluptates', 'repudiandae', 'sint', 'molestiae', 'non',
+  'recusandae', 'itaque', 'earum', 'rerum', 'hic', 'tenetur', 'a', 'sapiente',
+  'delectus', 'ut', 'aut', 'reiciendis', 'voluptatibus', 'maiores', 'alias',
+  'consequatur', 'aut', 'perferendis', 'doloribus', 'asperiores', 'repellat',
   'lorem', 'ipsum', 'dolor', 'sit', 'amet', 'consectetur', 'adipiscing', 'elit',
   'sed', 'do', 'eiusmod', 'tempor', 'incididunt', 'ut', 'labore', 'et', 'dolore',
-  'magna', 'aliqua', 'enim', 'ad', 'minim', 'veniam', 'quis', 'nostrud',
-  'exercitation', 'ullamco', 'laboris', 'nisi', 'aliquip', 'ex', 'ea', 'commodo',
-  'consequat', 'duis', 'aute', 'irure', 'in', 'reprehenderit', 'voluptate',
-  'velit', 'esse', 'cillum', 'fugiat', 'nulla', 'pariatur', 'excepteur', 'sint',
-  'occaecat', 'cupidatat', 'non', 'proident', 'sunt', 'culpa', 'qui', 'officia',
-  'deserunt', 'mollit', 'anim', 'id', 'est', 'laborum', 'ac', 'Integer', 'libero',
-  'felis', 'massa', 'justo', 'odio', 'cursus', 'metus', 'vitae', 'ultricies',
-  'praesent', 'lectus', 'tortor', 'condimentum', 'lacinia', 'quis', 'vel',
-  'eros', 'donec', 'odio', 'justo', 'sollicitudin', 'tellus', 'cras', 'blandit',
-  'mi', 'nisi', 'porta', 'a', 'lacinia', 'sapien', 'quisque', 'porta', 'nisl',
-  'eget', 'eleifend', 'quam', 'donec', 'pellentesque', 'diam', 'quis', 'arcu',
-  'curabitur', 'aliquam', 'quam', 'quis', 'nibh', 'cras', 'suscipit', 'massa',
-  'at', 'zara', 'semper', 'interdum', 'malesuada', 'fames', 'ac', 'ante',
-  'ipsum', 'primis', 'faucibus', 'orci', 'luctus', 'ultrices', 'posuere', 'cubilia',
-  'curae', 'mauris', 'viverra', 'diam', 'vitae', 'quam', 'hendrerit', 'lectus',
-  'morbi', 'quis', 'eros', 'nam', 'tincidunt', 'lacus', 'nunc', 'molestie',
-  'massa', 'sed', 'elementum', 'tempus', 'semper', 'est', 'ligula', 'vitae',
-  'risus', 'nulla', 'facilisi', 'cras', 'fermentum', 'odio', 'feugiat', 'pretium',
-  'libero', 'nunc', 'hendrerit', 'ante', 'Suspendisse', 'potenti', 'sed',
-  'leo', 'eget', 'magna', 'semper', 'vitae', 'porta', 'lorem', 'ipsum',
+  'magna', 'aliqua', 'ut', 'enim', 'ad', 'minim', 'veniam', 'quis', 'nostrud',
+  'exercitation', 'ullamco', 'laboris', 'nisi', 'ut', 'aliquip', 'ex', 'ea',
+  'commodo', 'consequat', 'duis', 'aute', 'irure', 'dolor', 'in', 'reprehenderit',
+  'in', 'voluptate', 'velit', 'esse', 'cillum', 'dolore', 'eu', 'fugiat', 'nulla',
+  'pariatur', 'excepteur', 'sint', 'occaecat', 'cupidatat', 'non', 'proident',
+  'sunt', 'in', 'culpa', 'qui', 'officia', 'deserunt', 'mollit', 'anim', 'id',
+  'est', 'laborum', 'lorem', 'ipsum', 'dolor', 'sit', 'amet', 'consectetur',
+  'adipiscing', 'elit', 'sed', 'do', 'eiusmod', 'tempor', 'incididunt', 'ut',
+  'labore', 'et', 'dolore', 'magna', 'aliqua', 'enim', 'ad', 'minim', 'veniam',
+  'quis', 'nostrud', 'exercitation', 'ullamco', 'laboris', 'nisi', 'aliquip', 'ex',
+  'ea', 'commodo', 'consequat', 'duis', 'aute', 'irure', 'dolor', 'in',
+  'reprehenderit', 'voluptate', 'velit', 'esse', 'cillum', 'dolore', 'eu', 'fugiat',
+  'nulla', 'pariatur', 'excepteur', 'sint', 'occaecat', 'cupidatat', 'non',
+  'proident', 'sunt', 'in', 'culpa', 'qui', 'officia', 'deserunt', 'mollit', 'anim',
+  'id', 'est', 'laborum',
+];
+
+const STANDARD_PARAGRAPHS = [
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+
+  'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.',
+
+  'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.',
+
+  'Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.',
+
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+
+  'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.',
+
+  'Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.',
+
+  'At vero eos et accusamus iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi.',
+
+  'Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae.',
+
+  'Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
 ];
 
 function getRandomElements<T>(arr: T[], count: number): T[] {
@@ -34,7 +115,27 @@ function generateWords(count: number): string {
   return words.join(' ');
 }
 
-const CLASSIC_START = 'Lorem ipsum dolor sit amet';
+function generateStandardSentences(count: number): string[] {
+  const sentences: string[] = [];
+  const allSentences = STANDARD_PARAGRAPHS.flatMap((p) =>
+    p.split(/(?<=[.!?])\s+/).filter((s) => s.trim()),
+  );
+
+  const shuffled = [...allSentences].sort(() => Math.random() - 0.5);
+  const selected = shuffled.slice(0, count);
+
+  for (let i = 0; i < count; i++) {
+    if (i < selected.length) {
+      sentences.push(selected[i]);
+    } else {
+      let sentence = generateWords(8);
+      sentence = sentence.replace(/\.$/, '') + '.';
+      sentences.push(sentence);
+    }
+  }
+
+  return sentences;
+}
 
 export interface LoremOptions {
   paragraphs: number;
@@ -56,25 +157,20 @@ export function generateLorem(options: Partial<LoremOptions> = {}): string {
   const paragraphsArr: string[] = [];
 
   for (let p = 0; p < paragraphs; p++) {
-    const sentences: string[] = [];
-    for (let i = 0; i < sentencesPerParagraph; i++) {
-      let sentence: string;
-      if (p === 0 && i === 0 && startWithClassic) {
-        const remainingWords = wordsPerSentence - CLASSIC_START.split(' ').length;
-        if (remainingWords > 0) {
-          const extraWords = getRandomElements(LOREM_WORDS, remainingWords).join(' ');
-          sentence = CLASSIC_START + ' ' + extraWords;
-        } else {
-          sentence = CLASSIC_START;
-        }
-      } else {
-        sentence = generateWords(wordsPerSentence);
-      }
-      sentence = sentence.replace(/\.$/, '') + '.';
-      if (p === 0 && i === 0 && startWithClassic) {
-        sentence = sentence.charAt(0).toUpperCase() + sentence.slice(1);
-      }
-      sentences.push(sentence);
+    let sentences: string[];
+
+    if (startWithClassic && p === 0) {
+      const classicText = STANDARD_PARAGRAPHS[0];
+      const classicWords = classicText.split(/\s+/).slice(0, wordsPerSentence).join(' ');
+      const firstSentence = classicWords.charAt(0).toUpperCase() + classicWords.slice(1);
+      const firstSentenceWithPeriod = firstSentence.endsWith('.')
+        ? firstSentence
+        : firstSentence + '.';
+
+      const remainingSentences = generateStandardSentences(sentencesPerParagraph - 1);
+      sentences = [firstSentenceWithPeriod, ...remainingSentences];
+    } else {
+      sentences = generateStandardSentences(sentencesPerParagraph);
     }
 
     if (outputFormat === 'list') {
