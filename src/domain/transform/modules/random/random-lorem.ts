ed69485d70422847a523bd6298b74485df9909d9
gausszhou <gausszhou@qq.com>
2026-05-11 00:20:04 +0800

    fix: 去除LOREM_WORDS中的重复词

--- src/domain/transform/modules/random/random-lorem.ts
+++ src/domain/transform/modules/random/random-lorem.ts
@@ -1,4 +1,4 @@
-const LOREM_WORDS = [
+const LOREM_WORDS_BASE = [
   'lorem', 'ipsum', 'dolor', 'sit', 'amet', 'consectetur', 'adipiscing', 'elit', 'sed',
   'do', 'eiusmod', 'tempor', 'incididunt', 'ut', 'labore', 'et', 'dolore', 'magna',
   'aliqua', 'enim', 'ad', 'minim', 'veniam', 'quis', 'nostrud', 'exercitation',
@@ -81,6 +81,8 @@ const LOREM_WORDS = [
   'proident', 'sunt', 'in', 'culpa', 'qui', 'officia', 'deserunt', 'mollit', 'anim',
   'id', 'est', 'laborum',
 ];
+
+const LOREM_WORDS = [...new Set(LOREM_WORDS_BASE)];
 
 const STANDARD_PARAGRAPHS = [
   'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
